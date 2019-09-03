149b257cd399653088c7c9c83c3618c5c56ebe7b
billzhang <361846718@qq.com>
2019-09-03 11:12:39 +0800

    DEV: Dispatch message without route match

--- router.go
+++ router.go
@@ -143,7 +143,7 @@ func (r *router) matchAndDispatch(messages <-chan *packets.PublishPacket, order 
 				m := messageFromPublish(message, client.ackFunc(message))
 				handlers := []MessageHandler{}
 				for e := r.routes.Front(); e != nil; e = e.Next() {
-					if e.Value.(*route).match(message.TopicName) {
+					//if e.Value.(*route).match(message.TopicName) {
 						if order {
 							handlers = append(handlers, e.Value.(*route).callback)
 						} else {
@@ -154,7 +154,7 @@ func (r *router) matchAndDispatch(messages <-chan *packets.PublishPacket, order 
 							}()
 						}
 						sent = true
-					}
+					//}
 				}
 				if !sent && r.defaultHandler != nil {
 					if order {
